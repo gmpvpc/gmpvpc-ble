a3ccf40aadf09aa1926b9e76f3f6ffa45b3808d9
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-02-06 01:54:23 +0100

    added cube display on web UI

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 import { DataSeries } from './data-series';
 
+/**
+ *
+ * EXPRESS AND SOCKET.IO CONF
+ *
+ */
+const express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.use('/public', express.static('front'));
+
+// io.on('connection', (data) => {
+//     numOfSockets++;
+//     io.sockets.emit('broadcast', "# of active sockets: " + numOfSockets);
+// });
+//
+// io.on('disconnection', (data) => {
+//     numOfSockets--;
+//     io.sockets.emit('broadcast', "# of active sockets: " + numOfSockets);
+// });
+
+server.listen(3000, () => {
+    console.log("App is listening on the port 3000")
+});
+
+/**
+ *
+ * SENSOR TAG I/O
+ *
+ */
+
 // var id = 'cc78ab7e7c84';
 const id = 'cc78ab7e8484';
 
 // var address = 'cc:78:ab:7e:7c:84';
 const address = 'cc:78:ab:7e:84:84';
 
-const reportPeriod = 500; // report period in milliseconds
+const reportPeriod = 200; // report period in milliseconds
 
-const recordDuration = 5000;// recording duration in milliseconds
+const recordDuration = 20000;// recording duration in milliseconds
 
 let async = require('async');
 
@@ -19,16 +55,21 @@ let SensorTag = require('sensortag');
 let AHRS = require('ahrs');
 
 let madgwick = new AHRS({
-    sampleInterval: 1/ reportPeriod / 1000, // Hz
-    algorithm: 'Madgwick',
+    sampleInterval: 5, // Hz
+    algorithm: 'Mahony',
     beta: 0.4,
     kp: 0.5,
+    ki: 0
 });
 
 const updateAHRS = () => {
     const p = dataSeries.getCurrentPoint();
     madgwick.update(p.gyro.x, p.gyro.y, p.gyro.z, p.accelero.x, p.accelero.y, p.accelero.z, p.magneto.x, p.magneto.y, p.magneto.z);
-    console.log(madgwick.toVector());
+    // madgwick.update(p.gyro.x, p.gyro.y, p.gyro.z    , p.accelero.x, p.accelero.y, p.accelero.z);
+    console.log(madgwick.getEulerAngles());
+    setTimeout(() => {``
+        io.sockets.emit('broadcast', madgwick.getEulerAngles()); // broadcast the vector to socket.io
+    },1);
 };
 
 let dataSeries = new DataSeries(updateAHRS);
@@ -93,10 +134,10 @@ SensorTag.discover(function(sensorTag) {
             callback();
         },
         function(callback) {
-            console.log("On patiente 5s ...");
-            setTimeout(function(){
-                callback();
-            }, recordDuration);
+            // console.log("On patiente 5s ...");
+            // setTimeout(function(){
+            //     callback();
+            // }, recordDuration);
         },
         function(callback) {
             console.log('Unnotifying accelerometer...');
