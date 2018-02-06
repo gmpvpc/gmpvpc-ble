5bfa5380ad3142eb78a79aa4c8986bf9813e40f3
Malah <malah@neuf.fr>
2018-02-06 23:08:18 +0100

    #87 added point dao

---
+++ src/dao/point/point-dao.js
@@ -0,0 +1,45 @@
+import {PointDaoConfig} from './point-dao-config'
+import {InfluxDB} from "influx/lib/src/index";
+
+export class PointDAO {
+    constructor() {
+        this.influxdb = null;
+        this.connect();
+    }
+
+    connect() {
+        this.influxdb = new InfluxDB({
+            host: PointDaoConfig.HOST,
+            database: PointDaoConfig.POINT_DB,
+            schema: [
+                {
+                    measurement: PointDaoConfig.POINT_MEASUREMENT,
+                    fields: PointDaoConfig.POINT_FIELDS,
+                    tags: PointDaoConfig.POINT_TAGS
+                }
+            ]
+        })
+    }
+
+    save(point, userId, callback) {
+        this.influxdb.writeMeasurement(PointDaoConfig.POINT_MEASUREMENT,
+            [{
+                tags: {
+                    [PointDaoConfig.USER_ID]: userId
+                },
+                fields: {
+                    [PointDaoConfig.POINT_FIELD_ACC_X]: point.accelero.x,
+                    [PointDaoConfig.POINT_FIELD_ACC_Y]: point.accelero.y,
+                    [PointDaoConfig.POINT_FIELD_ACC_Z]: point.accelero.z,
+                    [PointDaoConfig.POINT_FIELD_GYR_X]: point.gyro.x,
+                    [PointDaoConfig.POINT_FIELD_GYR_Y]: point.gyro.y,
+                    [PointDaoConfig.POINT_FIELD_GYR_Z]: point.gyro.z,
+                    [PointDaoConfig.POINT_FIELD_MAG_X]: point.magneto.x,
+                    [PointDaoConfig.POINT_FIELD_MAG_Y]: point.magneto.y,
+                    [PointDaoConfig.POINT_FIELD_MAG_Z]: point.magneto.z,
+                    [PointDaoConfig.DATE]: Date.now()
+                },
+            }]
+        ).then(callback);
+    }
+}
---
+++ src/model/point.js
@@ -0,0 +1,13 @@
+import {Coordinate} from "./coordinate";
+
+/**
+ * Cette classe permet de stocker l'ensemble des données retournées par les trois capteurs au même endroit
+ * Pour chaque "point", on aura 3 jeux de données: des valeurs pour l'accéleromètre, le gyroscope et le magnétomètre
+ */
+export class Point {
+    constructor() {
+        this.accelero = new Coordinate();
+        this.gyro = new Coordinate();
+        this.magneto = new Coordinate();
+    }
+}
